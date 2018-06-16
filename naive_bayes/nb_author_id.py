04dc93499a793cde7a89cafa9a776a34c79df586
Dave Rench McCauley <drench56@gmail.com>
2018-06-16 12:40:53 -0400

    feat: Train, predict, genereate an accuracy score, and time training vs. prediction of the Naive Bayes classifier (Gaussian)

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,7 +21,7 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+#print features_train[:5]
 
 
 #########################################################
@@ -29,6 +29,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 from sklearn.naive_bayes import GaussianNB
 
+#Setup the classifier
+clf = GaussianNB()
+
+#Train the classifer
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+#Predict an outcome, not necessary for the quiz at hand, but included for the sake of completeness
+t0 = time()
+predictions = clf.predict(features_test)
+print "testing time:", round(time()-t0, 3), "s"
+
+#Compute the accuracy of the trained classifer
+print "Accuracy: {}".format([clf.score(features_test, labels_test)])
 
 
 #########################################################
