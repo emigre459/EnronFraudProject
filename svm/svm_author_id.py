561c6acb9b000eee888e99e0ad7aa34a6129a986
Dave Rench McCauley <drench56@gmail.com>
2018-06-17 16:38:58 -0400

    feat: Setup SVM classifier, fit, and test (along with accuracy)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+clf = svm.SVC(kernel = 'linear')
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print "Training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+preds = clf.predict(features_test)
+print "Testing time:", round(time()-t0, 3), "s"
+
+print "Accuracy: ", accuracy_score(labels_test, preds)
+
 #########################################################
 
 
