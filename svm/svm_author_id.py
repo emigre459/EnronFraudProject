6cefcb5cdf814198479b00d359c4d57b169a687c
Dave Rench McCauley <drench56@gmail.com>
2018-06-17 17:40:58 -0400

    feat: Optimized C value for SVC and checked how many Chris predictions there were

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,7 +28,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
-clf = svm.SVC(kernel = 'linear')
+clf = svm.SVC(C = 10000.0, kernel = 'rbf')
+
+#Cut down training data set by 99% to see how it affects speed and accuracy
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
 
 t0 = time()
 clf.fit(features_train,labels_train)
@@ -39,6 +43,14 @@ preds = clf.predict(features_test)
 print "Testing time:", round(time()-t0, 3), "s"
 
 print "Accuracy: ", accuracy_score(labels_test, preds)
+
+print "Data classes: Sara = 0, Chris = 1"
+#print "Predicted data class for element 10: ", preds[10]
+#print "Predicted data class for element 26: ", preds[26]
+#print "Predicted data class for element 50: ", preds[50]
+
+#Since predictions for Sara result in 0, summing up will be the fastest way to identify Chris predictions
+print "Total number of Chris (1) predictions = ", preds.sum()
 
 #########################################################
 
